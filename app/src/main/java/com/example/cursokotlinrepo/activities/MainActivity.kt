f4836add48a0ac6c854c4ed0e335f45ea2c37f06
albertooofdz <119025836+albertooofdz@users.noreply.github.com>
2024-02-08 09:34:45 +0100

    Added a text in main activity

---
+++ app/src/main/java/com/example/cursokotlinrepo/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cursokotlinrepo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cursokotlinrepo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
